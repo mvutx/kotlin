caeb48abe1a9b319f4c59d5b1f18b1942d9a0bb7
vutii <giftmuuo.com@gmail.com>
2026-03-24 22:08:34 +0300

    update

---
+++ assignment1a.kt
@@ -0,0 +1,13 @@
+fun main(){
+    // addition
+    val number1 = 20
+    val number2 = 10
+    val sum = number1 + number2
+
+    println("Addition = $sum")
+    //
+
+    // subtraction  
+    val difference = number1 - number2
+    println("Difference = $difference")
+}
---
+++ lesson1a.kt
@@ -0,0 +1,18 @@
+fun main(){
+        // A variable is a container that stores data/values
+    // In kotlin we have two main types of initializers i.e: var and val
+    // val => immutable (unchangeable) but var => mutable (changeable)
+
+    val name = "Kivuti Gift" // string -> series of character inside quotes
+    val number1 = 5// interger-> whole number
+    val number2 = 40.6 // double -> deci,al
+    var letter = "k" // char-> single letter of alphabet
+    val isRegistered = true //boolean->it evaluates to either true or false.
+
+
+    println(name)
+    println(number1)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+}
