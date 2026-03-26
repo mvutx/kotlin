a7842d6488fde33a9d6fa4a867f1c7c277b1a015
vutii <giftmuuo.com@gmail.com>
2026-03-26 11:02:00 +0300

    update

--- assignment2.kt
+++ assignment2.kt
@@ -1,26 +1,17 @@
 //1. Using if statements or when statement, create a program to find if given year is leap or not 
 //2. Using if statements check if a given number is ODD or Even
 
-// 1
 fun main() {
+    // --- Leap Year Check ---
     val year = 2024
 
-    // Using if-else
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         println("$year is a leap year")
     } else {
         println("$year is not a leap year")
     }
 
-    // Using when statement
-    when {
-        (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) -> println("$year is a leap year")
-        else -> println("$year is not a leap year")
-    }
-}
-
-//2
-fun main() {
+    // --- Odd or Even Check ---
     val number = 15
 
     if (number % 2 == 0) {
